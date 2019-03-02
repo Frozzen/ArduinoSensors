a839476ca2534eefff5d5e1f875c373c8aca108b
Vald <goncharov.vlad@gmail.com>
2019-03-02 22:37:43 +0700

    отладил пробую запускать время рассвета заката - и прочие задачи по расписанию

--- mqtt/sunpos.py
+++ mqtt/sunpos.py
@@ -1,46 +1,73 @@
 # -*- coding: utf-8 -*-
-import datetime
+
 import astral
-import ephem
-import datetime
+import datetime, time
+import schedule
+
+from mymqtt import MyMQTT
+
+class MySunSched(object):
+    def __init__(self):
+        self.jobs = {}
+
+    def _astral(self):
+        """
+        print('Dawn:    %s' % str(sun['dawn']))
+        print('Sunrise: %s' % str(sun['sunrise']))
+        print('Noon:    %s' % str(sun['noon']))
+        print('Sunset:  %s' % str(sun['sunset']))
+        print('Dusk:    %s' % str(sun['dusk']))
+
+        :return: sun
+        """
+        a = astral.Astral()
+        a.solar_depression = 'nautical'
+        city = astral.Location(('Home', None, 54, 83, 'Asia/Novosibirsk', 0))
+        # print('Information for %s/%s\n' % ('Novosibirsk', city.region))
+        # timezone = city.timezone
+        # print('Timezone: %s' % timezone)
+        # print('Latitude: %.02f; Longitude: %.02f\n' % (city.latitude, city.longitude))
+        sun = city.sun(date=datetime.date.today()+datetime.timedelta(days = 1), local=True)
+        return sun
+
+    def sun_event(self, ev_type):
+        if ev_type in self.jobs:
+            schedule.cancel_job(self.jobs[ev_type])
+        sun = self._astral()
+        s = sun[ev_type].strftime("%H:%M:%S")
+        self.jobs[ev_type] = schedule.every().day.at(s).do(self.sun_event, ev_type)
+        # TODO send MQTT event
+        mqtt = MyMQTT()
+        mqtt.connect()
+        topic = mqtt.config.get('astral', 'sun_topic')
+        mqtt.client.publish(topic, ev_type, retain=True)
+        mqtt.close()
+
+    def day_event(self, ev_type):
+        # TODO send MQTT event
+        mqtt = MyMQTT()
+        mqtt.connect()
+        topic = mqtt.config.get('astral', 'day_topic')
+        mqtt.client.publish(topic, ev_type, retain=True)
+        mqtt.close()
+
+    def start(self):
+        self.sun_event('dawn')
+        self.sun_event('sunrise')
+        self.sun_event('sunset')
+        self.sun_event('dusk')
+
+        schedule.every().day.at('07:00').do(self.day_event, 'утро')
+        schedule.every().day.at('10:00').do(self.day_event, 'день')
+        schedule.every().day.at('19:00').do(self.day_event, 'вечер')
+        schedule.every().day.at('23:00').do(self.day_event, 'ночь')
 
 
-def _astral():
-    city_name = 'New York'
-    a = astral.Astral()
-    a.solar_depression = 'nautical'
-    city = astral.Location(('Home', None, 54, 83, 'Asia/Novosibirsk', 0))
-    # city = a[city_name]
-    print('Information for %s/%s\n' % (city_name, city.region))
-    timezone = city.timezone
-    print('Timezone: %s' % timezone)
-    print('Latitude: %.02f; Longitude: %.02f\n' % \
-        (city.latitude, city.longitude))
-    sun = city.sun(date=datetime.date.today(), local=True)
-    print('Dawn:    %s' % str(sun['dawn']))
-    print('Sunrise: %s' % str(sun['sunrise']))
-    print('Noon:    %s' % str(sun['noon']))
-    print('Sunset:  %s' % str(sun['sunset']))
-    print('Dusk:    %s' % str(sun['dusk']))
+def main():
+    MySunSched().start()
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
 
-def _ephem():
 
-    somewhere = ephem.Observer()
-    somewhere.lat = '54'  # <== change me
-    somewhere.lon = '83'  # <== and change me
-    somewhere.elevation = 120
-    print somewhere.date
-
-    sun = ephem.Sun()
-    r1 = somewhere.next_rising(sun)
-    s1 = somewhere.next_setting(sun)
-
-    somewhere.horizon = '-0:34'
-    r2 = somewhere.next_rising(sun)
-    s2 = somewhere.next_setting(sun)
-    print ("Visual sunrise %s" % r1)
-    print ("Visual sunset %s" % s1)
-    print ("Naval obs sunrise %s" % r2)
-    print ("Naval obs sunset %s" % s2)
-
-_astral()
+main()
