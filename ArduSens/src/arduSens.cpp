ad23d7337c70e7cfe2ccb029c258fd1b3f149772
Vald <goncharov.vlad@gmail.com>
2019-03-08 22:01:10 +0700

    сделал медленнее

--- ArduSens/src/arduSens.cpp
+++ ArduSens/src/arduSens.cpp
@@ -8,7 +8,7 @@
  * измеритель температуры + 4 контакта на замыкание
  * - температурных датчиков ds1820 может быть любое количество
  *  шлет только изменение опрос 30сек
- *   ArduStatHHHH/DS1820-XXXXXXXXXXXXXXX/INFO/resolution=val - один раз при старте
+  *   ArduStatHHHH/DS1820-XXXXXXXXXXXXXXX/INFO/resolution=val - один раз при старте
  *   ArduStatHHHH/DS1820-XXXXXXXXXXXXXXX/value=val раз в 30 сек при изменении
  *   ArduStatHHHH/light=val раз в 0.1 сек освещенность
  * - контакты опрашивает раз 100мсек
