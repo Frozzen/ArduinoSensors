2cb3520368d2cc8eaaa5a850f639f0aaa39cb208
Vald <goncharov.vlad@gmail.com>
2019-03-12 18:28:19 +0700

    накодировал вариант опроса

--- mqtt/mqtt-frwd.py
+++ mqtt/mqtt-frwd.py
@@ -3,12 +3,13 @@
 отправляем данные с com на mqtt. данные просто шлются с устройства.
     udev send sys/devices/platform/soc/20980000.usb/usb1/1-1/1-1.2/1-1.2:1.0/ttyUSB0/tty/ttyUSB0
 
-    mqtt-frwd.py /dev/ttyUSB0
+    mqtt-frwd.py [--debug|--jdy40] /dev/ttyUSB0
 
 """
 import fcntl
-import os
+import getopt
 import sys
+import syslog
 import time
 
 import serial
@@ -18,9 +19,9 @@ from mymqtt import MyMQTT
 MAX_LINE_LENGTH = 150
 __Connected = False
 
-import syslog
-
 DEBUG = False
+
+
 #################################################################
 def read_line_serial(ser, my_addr=':01'):
     """
@@ -28,14 +29,12 @@ def read_line_serial(ser, my_addr=':01'):
     строку из порта проверить что она послана на мой адрес, проверить CS
     'BadData' - за 150 символов не встретился перевод строки
 
+    :param my_addr:
     :param ser:
 
     :return: 'Ok', 'BadCS', 'NotForMe', 'BadData'
     """
 
-    line = ""
-    ok = False
-    cs = 0
     # ждем ':' - начало строки
     while True:
         ch = ser.read()
@@ -53,19 +52,19 @@ def read_line_serial(ser, my_addr=':01'):
     if ch != b'\n':
         return (line, 'BadData')
     # line = ser.readline()
-    if line[:3] != ':01':
+    if line[:3] != my_addr:
         return (line[:-3], 'NotForMe')
 
-    str = line[:-3]
+    s = line[:-3]
     cs = line[-2:]
     cs0 = 0
-    for ch in str:
+    for ch in s:
         cs0 += ord(ch)
     try:
         res = ((cs0 & 0xff)) + int(cs, 16)
     except ValueError:
-        return (str, 'BadCS')
-    return (str[3:], 'Ok' if res == 255 else 'BadCS')
+        return (s, 'BadCS')
+    return (s[3:], 'Ok' if res == 255 else 'BadCS')
 
 
 ###################################
@@ -78,25 +77,30 @@ def on_connect(client, userdata, flags, rc):
         print("Connection failed", rc, flags)
 
 
+class FrwdMQTT(MyMQTT):
+    def __init__(self, jdy40):
+        super(FrwdMQTT, self).__init__()
+        self.dump_msg_cnt = False
+        self.msg_cnt = self.bad_cs_cnt = self.not_for_me_cnt = self.bad_data_cnt = 0
+        self.TOPIC_START = ''
+        self.domitizc = {}
+        self.jdy_dev_list = []
+        self.TOPIC_START = self.config['MQTT']['topic_head']
+        self.domotizc = dict(self.config.items('Domotizc'))
+        if jdy40:
+            self.jdy_dev_list = self.config['jdy-40']['dev_list'].split(',')
 
+    ###################################
+    def read_data(self, ser):
+        """
+        один оборот опроса данных
 
-##########################################
-def main_loop(TOPIC_START, client, ser, domotizc):
-    """
-    основной цикл приложения
+        :param self:
+        :param ser:
+        :return:
+        """
+        global DEBUG
 
-    устойчивость по отпаданию COM и MQTT
-    запустить по таймеру печать bus/msg_cnt
-
-    :param domotizc:
-    :param TOPIC_START:
-    :param client:
-    :param ser:
-    :return:
-    """
-    __dump_msg_cnt = False
-    msg_cnt = bad_cs_cnt = not_for_me_cnt = bad_data_cnt = 0
-    while True:
         try:
             line, result = read_line_serial(ser)
         except (serial.SerialException, ValueError) as e:
@@ -105,64 +109,104 @@ def main_loop(TOPIC_START, client, ser, domotizc):
             if 'disconnected' in e.args[0]:
                 syslog.syslog(syslog.LOG_NOTICE, "mqtt-frwd error %s **" % (str(e),))
                 return
-            continue
+            return
         # print line,result
-        if __dump_msg_cnt:
-            __dump_msg_cnt = False
-            client.publish('tele/' + TOPIC_START + "bus/msg_cnt", str(msg_cnt).decode('utf-8'), retain=True)
+        if self.dump_msg_cnt:
+            self.dump_msg_cnt = False
+            self.client.publish('tele/' + self.TOPIC_START + "bus/msg_cnt", str(self.msg_cnt).decode('utf-8'),
+                                retain=True)
 
         if result == 'Ok':
             topic, val = line.split('=')
             if DEBUG:
                 print("t:%s = %s" % (topic, val))
             if "log" == topic:
-                client.publish('log/' + TOPIC_START, val.decode('utf-8'))
-                continue
+                self.client.publish('log/' + self.TOPIC_START, val.decode('utf-8'))
+                return
 
-            client.publish('stat/' + TOPIC_START + topic, val.decode('utf-8'), retain=True)
+            self.client.publish('stat/' + self.TOPIC_START + topic, val.decode('utf-8'), retain=True)
             topic = topic.lower()
-            if topic in domotizc:
-                tval = '{ "idx" : %s, "nvalue" : 0, "svalue": "%s" }' % (domotizc[topic], val.strip())
-                client.publish("domoticz/in", tval.decode('utf-8'))
-                __dump_msg_cnt = True
-            msg_cnt += 1
-            continue
-        __dump_msg_cnt = True
+            if topic in self.domotizc:
+                tval = '{ "idx" : %s, "nvalue" : 0, "svalue": "%s" }' % (self.domotizc[topic], val.strip())
+                self.client.publish("domoticz/in", tval.decode('utf-8'))
+                self.dump_msg_cnt = True
+            self.msg_cnt += 1
+            return
+        self.dump_msg_cnt = True
         syslog.syslog(syslog.LOG_NOTICE, "*bad: %s:%s**" % (result, line))
         if DEBUG:
             print("*bad:%s = %s" % (result, line))
         if result == 'BadCS':
-            bad_cs_cnt += 1
-            client.publish('tele/' + TOPIC_START + "bus/errors", str(bad_cs_cnt).decode('utf-8'), retain=True)
+            self.bad_cs_cnt += 1
+            self.client.publish('tele/' + self.TOPIC_START + "bus/errors", str(self.bad_cs_cnt).decode('utf-8'),
+                                retain=True)
         elif result == 'NotForMe':
-            not_for_me_cnt += 1
-            client.publish('tele/' + TOPIC_START + "bus/not4me", str(not_for_me_cnt).decode('utf-8'), retain=True)
+            self.not_for_me_cnt += 1
+            self.client.publish('tele/' + self.TOPIC_START + "bus/not4me", str(self.not_for_me_cnt).decode('utf-8'),
+                                retain=True)
         elif result == 'BadData':
-            bad_data_cnt += 1
-            client.publish('tele/' + TOPIC_START + "bus/bad_data", str(bad_data_cnt).decode('utf-8'), retain=True)
+            self.bad_data_cnt += 1
+            self.mqtt.client.publish('tele/' + self.TOPIC_START + "bus/bad_data",
+                                     str(self.bad_data_cnt).decode('utf-8'), retain=True)
+
+    ##########################################
+    def main_loop(self, ser):
+        """
+        основной цикл приложения
+
+        устойчивость по отпаданию COM и MQTT
+        запустить по таймеру печать bus/msg_cnt
+
+        :param ser:
+        :return:
+        """
+        ADDR_FROM = 0x1
+        SEND_CMD = 0x1
+        while True:
+            if len(self.jdy_dev_list) > 0:
+                for DEVICE_NO in self.jdy_dev_list:
+                    cmd = ":%02x%02x%02x;" % (DEVICE_NO, ADDR_FROM, SEND_CMD)
+                    ser.write(cmd)
+                    self.read_data(ser)
+            else:
+                self.read_data(ser)
 
 
 def main(argv):
-    global __Connected  # Use global variable
-    mqtt = MyMQTT()
+    global __Connected, DEBUG  # Use global variable
     port = '/dev/' + argv[1].split('/')[-1]
     fh = open(port, 'r')
     try:
         fcntl.flock(fh, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except:
-        os._exit(1)
+    except OSError:
+        sys.exit(1)
 
-    TOPIC_START = mqtt.config['MQTT']['topic_head']
-    domotizc = dict(mqtt.config.items('Domotizc'))
+    jdy40 = False
+    try:
+        opts, args = getopt.getopt(argv, "hdj", ["help", "debug", "jdy40"])
+    except getopt.GetoptError:
+        print(__doc__)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(__doc__)
+            sys.exit(1)
+        elif opt in ("-d", "--debug"):
+            DEBUG = True
+        elif opt in ("-j", "--jdy40"):
+            jdy40 = True
+
+    mqtt = FrwdMQTT(jdy40)
     syslog.syslog(syslog.LOG_NOTICE, "mqtt-frwd on %s started" % (argv[1]))
-    ser = serial.Serial(port=port, baudrate=mqtt.config['COM']['baudrate'], timeout = 0.1)
+    ser = serial.Serial(port=port, baudrate=mqtt.config['COM']['baudrate'], timeout=0.1)
     mqtt.connect()
-    main_loop(TOPIC_START, mqtt.client, ser, domotizc)
+    mqtt.main_loop(ser)
 
     syslog.syslog(syslog.LOG_NOTICE, "mqtt-frwd on %s stopped **" % (argv[1]))
     if DEBUG:
         print("mqtt-frwd on %s stopped **" % (argv[1]))
     ser.close()
+
 
 if __name__ == '__main__':
     syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_DAEMON)
