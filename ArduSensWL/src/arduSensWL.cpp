6453705e9351206c9cf37f4dc6298045b6571b2c
Vald <goncharov.vlad@gmail.com>
2019-03-12 11:15:53 +0700

    гото к отладке сервера

--- ArduSensWL/src/arduSensWL.cpp
+++ ArduSensWL/src/arduSensWL.cpp
@@ -8,18 +8,19 @@
 #include <CircularBuffer.h>
 
 /*
+ * через JDY40 рабоает по опросу, на serial рабоатет - просто печать
  * измеритель температуры + 4 контакта на замыкание
  * - температурных датчиков ds1820 может быть любое количество
  *  шлет только изменение опрос 30сек
- *   ArduStatHHHH/DS1820-XXXXXXXXXXXXXXX/temp=val раз в 30 сек при изменении
- *   ArduStatHHHH/larch-1=val раз в 0.1 сек контакт
- *   ArduStatHHHH/light=val раз в 0.1 сек освещенность
+ *   ArduStatHH/DS1820-XXXXXXXXXXXXXXX/temp=val раз в 30 сек при изменении
+ *   ArduStatHH/larch-1=val раз в 0.1 сек контакт
+ *   ArduStatHH/light=val раз в 0.1 сек освещенность
  * - контакты опрашивает раз 100мсек
- *   ArduStatHHHH/latch-1=val раз в 0.1 сек при изменении
- *   ArduStatHHHH/latch-4=val раз в 0.1 сек при изменении
+ *   ArduStatHH/latch-1=val раз в 0.1 сек при изменении
+ *   ArduStatHH/latch-4=val раз в 0.1 сек при изменении
  *  шлет только изменения
  * - раз в 5 секунд шлет keeralive
- *    ArduStatHHHH/INFO/alive=cnt раз в 5
+ *    ArduStatHH/INFO/alive=cnt раз в 5
  */
 
 
