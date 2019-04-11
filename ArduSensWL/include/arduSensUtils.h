a055961233ce901e6bbd76671a97a0e165eb5e4e
Frozzen <ya.goncharo2009@yandex.ru>
2019-04-11 10:57:07 +0700

    запустил WL не работает temp

--- ArduSensWL/include/arduSensUtils.h
+++ ArduSensWL/include/arduSensUtils.h
@@ -3,9 +3,14 @@
 #define HOST_ADDR ":01"
 
 void sendToServer(const char *r, bool send=false);
+#ifndef NO_TEMP
 const char *getAddrString(DeviceAddress &dev);
 void setupArduSens();
 void doTestContacts();
 bool doSendTemp();
+#endif
 void doAlive();
 extern uint16_t s_time_cnt;
+
+#define ADDR_STR HOST_ADDR SENSOR_NAME DEVICE_NO
+extern char s_buf[MAX_OUT_BUFF];  
