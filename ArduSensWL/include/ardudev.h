076b359ab8d038d5403918ed0e43eee058438d8f
Frozzen <ya.goncharo2009@yandex.ru>
2019-04-12 12:16:38 +0700

    сделал почаще вывод для отладки

--- ArduSensWL/include/ardudev.h
+++ ArduSensWL/include/ardudev.h
@@ -1,7 +1,7 @@
 #define SENSOR_NAME "ArduStat"
 #define DEVICE_NO "02"
 // Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 12
 #define FOTO_SENSOR A1
 // чило сканируемых pin начиная с PIN3-PIN7
 #define IN_PIN_START 3
@@ -23,4 +23,4 @@
 #define ALT_RS232_TX 9
 
 // нужно изменять между образцами - чтобы было поменьше коллизий
-#define DO_MSG_RATE 5000
+#define DO_MSG_RATE 100
